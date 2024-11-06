de2a85630e0f0658cfc5e247ddf68cb0b7d73a80
Edrard A <demonand@gmail.com>
2024-11-06 16:08:29 +0200

    feat: updater
    Adding updater script

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+db=$(<db.json)
+config=$(<config.php)
+user=$(stat -c '%U' db.json)
+group=$(stat -c '%G' db.json)
+
+git_version=$(git ls-remote https://github.com/Edrard/bconf.git HEAD | cut -f1)
+local_version=$(git rev-parse HEAD)
+if [[ ${local_version} != ${git_version} ]]; then
+    git fetch --all
+    git reset --hard origin/master
+    git pull
+fi
+
+export COMPOSER_ALLOW_SUPERUSER=1
+composer self-update --2
+yes | composer install --no-dev
+export COMPOSER_ALLOW_SUPERUSER=0
+
+echo $db > db.json
+echo $config > config.php
+
+chown ${user}:${group} * -R
