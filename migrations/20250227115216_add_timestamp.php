2c2334fb636700d50c0cc780583da18e1661d9d7
Edrard A <demonand@gmail.com>
2025-02-27 12:09:19 +0200

    feat: Adding timestamps

---
+++ migrations/20250227115216_add_timestamp.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+use Phoenix\Migration\AbstractMigration;
+
+final class AddTimestamp extends AbstractMigration
+{
+    protected function up(): void
+    {
+
+        $set['default'] = NULL;
+        $set['null'] = TRUE;
+
+        $this->table('group', 'id')
+        ->addColumn('created_at', 'timestamp',$set)
+        ->save();
+        $this->table('group', 'id')
+        ->addColumn('updated_at', 'timestamp',$set)
+        ->save();
+        $this->table('devices_config', 'id')
+        ->addColumn('created_at', 'timestamp',$set)
+        ->save();
+        $this->table('devices_config', 'id')
+        ->addColumn('updated_at', 'timestamp',$set)
+        ->save();
+        $this->table('type', 'id')
+        ->addColumn('created_at', 'timestamp',$set)
+        ->save();
+        $this->table('type', 'id')
+        ->addColumn('updated_at', 'timestamp',$set)
+        ->save();
+    }
+
+    protected function down(): void
+    {
+        $this->execute('ALTER TABLE `type` DROP `created_at`;');
+        $this->execute('ALTER TABLE `group` DROP `created_at`;');
+        $this->execute('ALTER TABLE `devices_config` DROP `created_at`;');
+        $this->execute('ALTER TABLE `type` DROP `updated_at`;');
+        $this->execute('ALTER TABLE `group` DROP `updated_at`;');
+        $this->execute('ALTER TABLE `devices_config` DROP `updated_at`;');
+    }
+}
