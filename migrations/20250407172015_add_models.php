ce2088818f45ffefee146e8e7a637970e6216a22
Edrard A <demonand@gmail.com>
2025-04-07 17:51:09 +0300

    fix: fixing migration

--- migrations/20250407172015_add_models.php
+++ migrations/20250407172015_add_models.php
@@ -9,27 +9,34 @@ final class AddModels extends AbstractMigration
     protected function up(): void
     {
         $date = date("Y-m-d H:i:s");
-        $this->insert('model',[
-            'model' => 'cisco',
-            'description' => 'Cisco',
-            'created_at' => $date,
-            'updated_at' => $date,
-            ]
-        );
-        $this->insert('model',[
-            'model' => 'mikrotik',
-            'description' => 'Mikrotik',
-            'created_at' => $date,
-            'updated_at' => $date,
-            ]
-        );
-        $this->insert('model',[
-            'model' => 'mikrotikkeys',
-            'description' => 'Mikrotik with show-sensitive',
-            'created_at' => $date,
-            'updated_at' => $date,
-            ]
-        );
+
+        if($this->select("SELECT * FROM `model` WHERE model = 'cisco';") === []){
+            $this->insert('model',[
+                'model' => 'cisco',
+                'description' => 'Cisco',
+                'created_at' => $date,
+                'updated_at' => $date,
+                ]
+            );
+        }
+        if($this->select("SELECT * FROM `model` WHERE model = 'mikrotik';") === []){
+            $this->insert('model',[
+                'model' => 'mikrotik',
+                'description' => 'Mikrotik',
+                'created_at' => $date,
+                'updated_at' => $date,
+                ]
+            );
+        }
+        if($this->select("SELECT * FROM `model` WHERE model = 'mikrotikkeys';") === []){
+            $this->insert('model',[
+                'model' => 'mikrotikkeys',
+                'description' => 'Mikrotik with show-sensitive',
+                'created_at' => $date,
+                'updated_at' => $date,
+                ]
+            );
+        }
     }
 
     protected function down(): void
